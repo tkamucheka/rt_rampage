6172ec2eeb7318301feed9c2b0d9af99f2bc70c9
Tendayi Kamucheka <ftendayi@outlook.com>
2017-12-16 19:08:30 +0800

    Updates docs and adds new functions to ChainWalker module.

--- apps/chain_walker/lib/chain_walker.ex
+++ apps/chain_walker/lib/chain_walker.ex
@@ -86,7 +86,7 @@ defmodule ChainWalker do
   def set_charset_opts(chain_walker_context, s_charset, n_min_length, n_max_length) do
     set_charset(chain_walker_context, s_charset)
     |> set_min_password_length(n_min_length, n_max_length)
-    |> set_max_password_length(n_max_length)
+    |> set_max_password_length(n_min_length, n_max_length)
   end
 
   def set_charset(chain_walker_context, s_charset) do
@@ -103,6 +103,7 @@ defmodule ChainWalker do
   ## Parameters
     - `chain_walker_context`: %ChainWalkerContext{}
     - `n_min_length`: Minimum length for plain text passwords.
+    - `n_max_length`: Maximum length for plain text passwords.
 
   ## Returns
     - `%ChainWalkerContext{}`
@@ -156,12 +157,83 @@ defmodule ChainWalker do
     end
   end
 
-  
-  def set_max_password_length(chain_walker_context, n_max_length) do
+  @doc """
+  Set maximum plain text password length to be included in the rainbow table.
 
+    ## Parameters
+      - `chain_walker_context`: %ChainWalkerContext{}
+      - `n_min_length`: Minimum length for plain text passwords.
+      - `n_max_length`: Maximum length for plain text passwords.
+
+    ## Returns
+      - `%ChainWalkerContext{}`
+
+    ## Examples
+        iex> ChainWalker.set_max_password_length(%ChainWalkerContext{}, 1, 2)
+        %ChainWalkerContext{charset: nil, hash_routine: nil, max_length: 2, min_length: nil, table_index: nil}
+  """
+  def set_max_password_length(chain_walker_context, n_min_length, n_max_length) do
+    case validate_max_password_length(n_min_length, n_max_length) do
+      { :ok, max_length } -> %{ chain_walker_context | max_length: max_length }
+      { _, reason }       -> raise ArgumentError, message: reason
+    end
   end
 
-  def set_table_index(%ChainWalkerContext{}, n_table_index) do
-    n_table_index
+  @doc """
+  Validates correctness of maximum password length argument.
+
+  ## Parameters
+    - `n_min_length`: Minimum plain text password length.
+    - `n_max_length`: Maximum plain text password length.
+
+  ## Returns
+    - `{ :ok, n_max_length }`
+    - `{ :error, reason }`
+
+  ## Examples
+      iex> ChainWalker.validate_max_password_length(1, 2)
+      {:ok, 2}
+      iex> ChainWalker.validate_max_password_length(2, 1)
+      {:error, "Bad argument: Maximum password length (1) cannot be lesser than minimum length (2)."}
+      iex> ChainWalker.validate_max_password_length(0, 0)
+      {:error, "Bad argument: Maximum password length (0) cannot be zero or less."}
+      iex> ChainWalker.validate_max_password_length(1, 10)
+      {:error, "Bad argument: Maximum password length (10) cannot be greater than maximum supported password length (9)." }
+  """
+  def validate_max_password_length(n_min_length, n_max_length) do
+    cond do
+      # Check maximum length is not lesser than minimum length
+      n_min_length > n_max_length ->
+        { :error, "Bad argument: Maximum password length (#{n_max_length}) cannot be lesser than minimum length (#{n_min_length})." }
+      # Check maximum password length is greater than zero
+      n_max_length <= 0           ->
+        { :error, "Bad argument: Maximum password length (#{n_max_length}) cannot be zero or less." }
+      # Check maximum password length is not more than supported length
+      n_max_length >= 10          ->
+        { :error, "Bad argument: Maximum password length (#{n_max_length}) cannot be greater than maximum supported password length (9)." }
+      # Otherwise return :ok
+      true                        ->
+        { :ok, n_max_length }
+    end
+  end
+
+  @doc """
+  Set table index for current operation.
+
+  ## Parameters
+    - `chain_walker_context`: %ChainWalkerContext{}
+    - `n_table_index`: Positive integer indicating table index
+
+  ## Returns
+    - `%ChainWalkerContext{}`
+
+  ## Examples
+      iex> ChainWalker.set_table_index(%ChainWalkerContext{}, 0)
+      %ChainWalkerContext{charset: nil, hash_routine: nil, max_length: nil,
+       min_length: nil, table_index: 0}
+  """
+  def set_table_index(chain_walker_context, n_table_index) do
+    if n_table_index < 0, do: raise ArgumentError, message: "Table index (#{n_table_index}) cannot be less than zero (0)."
+    %{ chain_walker_context | table_index: n_table_index }
   end
 end
