c82b7486c958ab7997d07637bd4cd7928e376ecd
Tendayi Kamucheka <ftendayi@outlook.com>
2018-01-13 13:39:21 +0800

    Adds more ChainWalkerContext variables

--- apps/chain_walker/lib/chain_walker_context.ex
+++ apps/chain_walker/lib/chain_walker_context.ex
@@ -1,3 +1,3 @@
 defmodule ChainWalkerContext do
-  defstruct [:hash_routine, :charset, :min_length, :max_length, :table_index, :charset_length, :plain_space_upto_x, :plain_space_total, :m_n_index]
+  defstruct [:m_hash_routine, :s_charset, :n_min_length, :n_max_length, :n_table_index, :n_charset_length, :n_plain_space_upto_x, :n_plain_space_total, :n_index, :s_plain, :n_plain_length, :s_hash, :n_reduce_offset]
 end
