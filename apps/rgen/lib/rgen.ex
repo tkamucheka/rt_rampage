4c77b0f577ffd58e9cb9d9957f3eff6e3d7070a4
Tendayi Kamucheka <ftendayi@outlook.com>
2018-01-13 13:29:45 +0800

    Adds arg parsing and process method

--- apps/rgen/lib/rgen.ex
+++ apps/rgen/lib/rgen.ex
@@ -15,11 +15,22 @@ defmodule Rgen do
   """
   def parse_args(args) do
     # Default options
-    options = %{}
+    options = %{ filename_suffix: "" }
     # Parse args
     cmd_opts = OptionParser.parse(
       args,
-      switches: [help: :boolean, hashtype: :string],
+      switches: [
+        help: :boolean,
+        hashtype: :string,
+        charset: :string,
+        minlength: :integer,
+        maxlength: :integer,
+        chainlength: :integer,
+        numchains: :integer,
+        tableindex: :integer,
+        filename_suffix: :string,
+        benchmark: :boolean
+        ],
       aliases: [h: :help]
     )
 
@@ -70,6 +81,112 @@ defmodule Rgen do
   end
 
   def process({options, args}) do
+    IO.inspect options
+    IO.inspect args
 
+    if Enum.count(options) == 7 do
+      if options.benchmark do
+        Benchmark.run(options.hashtype, options.charset, options.minlength, options.maxlength, options.tableindex)
+      end
+    end
+
+    unless Enum.count(options) == 8 do
+      IO.puts @moduledoc
+      System.halt(0)
+    end
+
+    s_hash_routine  = options.hashtype
+    s_charset       = options.charset
+    n_min_length    = options.minlength
+    n_max_length    = options.maxlength
+    n_table_index   = options.tableindex
+
+    n_chain_length  = options.chainlength
+    n_numchains     = options.numchains
+    s_filename_suffix = options.filename_suffix
+
+    # n_numchains check
+    if n_numchains >= 134217728 do
+      IO.puts "This will generate a table larger than 2GB, which is not supported."
+      IO.puts "Please use a smaller number of chains, (--numchains), less than 134217728."
+    end
+
+    # Setup ChainWalkerContext
+    chain_walker_context =
+      ChainWalker.set_hash_routine(s_hash_routine)
+      |> ChainWalker.set_charset_opts(s_charset, n_min_length, n_max_length)
+      |> ChainWalker.set_table_index(n_table_index)
+      # ChainWalker.dump
+
+    IO.inspect chain_walker_context
+
+    s_filename = "#{s_hash_routine}_#{s_charset}##{n_min_length}_#{n_max_length}_#{n_table_index}_#{n_chain_length}x#{n_numchains}_#{s_filename_suffix}.rt"
+
+    case File.open(s_filename, [:append, :binary]) do
+      { :ok, file }       -> File.close(file)
+      { :error, reason }  -> raise File.Error, message: reason
+    end
+
+    file =
+      case File.open(s_filename, [:read, :write, :binary]) do
+        { :ok, file } -> file
+        { :error, reason } -> raise File.Error, message: reason
+      end
+
+    data_length = IO.binread(file, :all) |> String.length
+
+    n_data_length = div(data_length, 16 * 16)
+
+    if n_data_length == n_numchains * 16 do
+      IO.puts "Precomputation of this rainbow table already finished"
+      File.close(file)
+      System.halt(0)
+    end
+
+    if n_data_length > 0 do
+      IO.puts "Continuing from interrupted precomputation..."
+    end
+
+    IO.puts "Generating..."
+
+    start_time = System.system_time
+    for index <- div(n_data_length, 16)..n_numchains do
+      chain_walker_context =
+        ChainWalker.generate_random_index(chain_walker_context)
+
+      # n_index = chain_walker_context.n_index
+
+      IO.binwrite file, <<chain_walker_context.n_index::32>>
+
+      chain_walker_context = step(chain_walker_context, n_chain_length, n_numchains)
+
+      IO.binwrite file, <<chain_walker_context.n_index::32>>
+
+      if rem(index+1, 100000) == 0 || index+1 == n_numchains do
+        finish_time = System.system_time
+        n_second = div((finish_time - start_time), 100000000)
+        IO.puts "#{index+1} of #{n_numchains} rainbow chains generated (#{div(n_second, 60)} m #{rem(n_second, 60)} s)"
+      end
+    end
+
+    File.close(file)
+  end
+
+  def step(chain_walker_context, n_chain_length, n_numchains) do
+    ChainWalker.index_to_plain(chain_walker_context)
+    |> ChainWalker.plain_to_hash
+    |> ChainWalker.hash_to_index(0)
+    |> step(n_chain_length, n_numchains, 0)
+  end
+
+  def step(chain_walker_context, n_chain_length, n_numchains, index) do
+    unless index >= n_numchains do
+      ChainWalker.index_to_plain(chain_walker_context)
+      |> ChainWalker.plain_to_hash
+      |> ChainWalker.hash_to_index(index)
+      |> step(n_chain_length, index)
+    end
+
+    chain_walker_context
   end
 end
